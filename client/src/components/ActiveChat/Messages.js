6a781c1f0238f31b2fa66d2d7bff7f007f62fe76
Garnet <garnetbordieri@hotmail.com>
2021-09-13 15:46:44 -0400

    Fixed the bug in Issue #1
    Added a selector function to Messages.js so that the component will update when the app's state changes. Sorted the messages by creation time before displaying them.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
+import { connect, useSelector } from "react-redux";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+  const { messages, otherUser, userId } = useSelector(messagesSelector);
 
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.sort((messageA, messageB) => (messageA.createdAt > messageB.createdAt) ? 1 : -1).map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
@@ -19,6 +20,19 @@ const Messages = (props) => {
       })}
     </Box>
   );
+};
+
+const messagesSelector = (state) => {
+  const conversation = 
+  state.conversations &&
+  state.conversations.find(
+    (conversation) => conversation.otherUser.username === state.activeConversation
+  )
+  return { 
+    messages : conversation.messages, 
+    otherUser : conversation.otherUser,
+    userId : state.user.id
+  };
 };
 
 export default Messages;
